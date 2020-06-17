b707607164bf8a64e09b1547346b9ed8b754e13e
winne <554808642@qq.com>
2020-06-18 00:10:56 +0800

    8

---
+++ resources/views/layouts/default.blade.php
@@ -0,0 +1,20 @@
+<!DOCTYPE html>
+<html>
+<head>
+    <title>@yield('title', 'Weibo App') - Laravel 入门教程</title>
+    <link rel="stylesheet" href="{{ mix('css/app.css') }}">
+    <script src="{{ mix('js/app.js') }}"></script>
+</head>
+<body>
+@include('layouts._header')
+<div class="container">
+    <div class="offset-md-1 col-md-10">
+@include('shared._messages')
+@yield('content')
+        @include('layouts._footer')
+    </div>
+</div>
+<script src="{{ mix('js/app.js') }}"></script>
+</body>
+</html>
+
